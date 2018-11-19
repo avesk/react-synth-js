e47d58f1f91f0a919a4b7579996b84b1a6f38e10
avesk <averykushner@gmail.com>
2018-11-18 19:44:42 -0800

    added some preset tambers and a bit of skeleton code for a wave analyser

--- react-synth-js/src/app/Synth/Synth.js
+++ react-synth-js/src/app/Synth/Synth.js
@@ -30,39 +30,98 @@ const keyboardFrequencyMap = {
     '85': 987.7666, // B
 }
 
-// const tamberMode = [ // square wave
-//     {'harmonic': 1, 'gain': 1},
-//     {'harmonic': 3, 'gain': .5},
-//     {'harmonic': 5, 'gain': .25},
-//     {'harmonic': 7, 'gain': .125},
-//     {'harmonic': 8, 'gain': .065},
-//     // {'harmonic': 1, 'gain': 1},
-//     // {'harmonic': 2, 'gain': .5},
-//     // {'harmonic': 3, 'gain': .7},
-//     // {'harmonic': 4, 'gain': .125},
-//     // {'harmonic': 5, 'gain': .065},
-// ];
-
 // SET UP AUDIO CONTEXT
 const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
+// create audio analyiser
+const analyser = audioCtx.createAnalyser();
+
 // PROCESSING CHAIN
 const masterGain = audioCtx.createGain(); // node
+
+class Analyser extends Component {
+    constructor(props) {
+        super(props);
+
+        // CONNECTIONS
+        analyser.connect(audioCtx.destination);
+
+        // what does this do?
+        analyser.fftSize = 2048;
+
+        // what does this do exactly?
+        var bufferLength = analyser.frequencyBinCount;
+
+        var dataArray = new Uint8Array(bufferLength);
+
+        this.state = {bufferLength: bufferLength, dataArray: dataArray};
+    }
+
+    renderFrequencyData() {
+        const bufferLength = this.state.bufferLength;
+        const dataArray = this.state.dataArray;
+        analyser.getByteFrequencyData(dataArray);
+        console.log(dataArray);
+        // for (var i = 0; i < bufferLength; i++) {
+        //     console.log(dataArray[i]); 
+        // }
+    }
+
+    render() {
+        this.renderFrequencyData();
+        return <p>Analyser</p>;
+    }
+}
+
+const presets = {
+    'sine': [
+        {'harmonic': 1, 'gain': 1},
+    ],
+    'square': [
+        {'harmonic': 1, 'gain': 1},
+        {'harmonic': 3, 'gain': .5},
+        {'harmonic': 5, 'gain': .25},
+        {'harmonic': 7, 'gain': .125},
+        {'harmonic': 8, 'gain': .065},
+    ],
+    'triangle': [
+        {'harmonic': 1, 'gain': 1},
+        {'harmonic': 3, 'gain': -1*(1/Math.sqrt(3))},
+        {'harmonic': 5, 'gain': (1/Math.sqrt(5))},
+        {'harmonic': 7, 'gain': -1*(1/Math.sqrt(7))},
+        {'harmonic': 8, 'gain': (1/Math.sqrt(8))},
+    ],
+    'p_fith': [
+        {'harmonic': 3, 'gain': 1},
+        {'harmonic': 2, 'gain': 1},
+    ],
+    'p_fourth': [
+        {'harmonic': 4, 'gain': 1},
+        {'harmonic': 3, 'gain': 1},
+    ],
+    'maj_third': [
+        {'harmonic': 6, 'gain': 1},
+        {'harmonic': 5, 'gain': 1},
+    ],
+    'min_third': [
+        {'harmonic': 5, 'gain': 1},
+        {'harmonic': 4, 'gain': 1},
+    ],
+    'maj_triad': [
+        {'harmonic': 6, 'gain': 1},
+        {'harmonic': 5, 'gain': 1},
+        {'harmonic': 4, 'gain': 1},
+    ]
+}
 
 class Synth extends Component {
 
     constructor(props) {
         super(props);
 
-        const presetTamber = [
-            {'harmonic': 1, 'gain': 1},
-            {'harmonic': 3, 'gain': .5},
-            {'harmonic': 5, 'gain': .25},
-            {'harmonic': 7, 'gain': .125},
-            {'harmonic': 8, 'gain': .065},
-        ];
+        const presetTamber = presets.triangle;
 
-        this.state = {activeOscillators: {}, tamberMode: presetTamber, waveform: 'triangle' };
+        this.state = {activeOscillators: {}, tamberMode: presetTamber, waveform: 'sine' };
 
         // CONNECTIONS
         masterGain.connect(audioCtx.destination);
@@ -131,13 +190,19 @@ class Synth extends Component {
         const harmGain = audioCtx.createGain(); // node
         harmGain.gain.setValueAtTime(gain, audioCtx.currentTime);
         activeOscillators[offset].connect(harmGain);
+        activeOscillators[offset].connect(analyser); // connect analyser
         harmGain.connect(masterGain);
         activeOscillators[offset].start();
         this.setState({activeOscillators: activeOscillators});
     }
 
     render() {
-        return <p>SYNTH</p>;
+        return(
+            <React.Fragment>
+                <Analyser />
+                <p>SYNTH</p>
+            </React.Fragment>
+        ) 
     }
 }
 
---
+++ sndpeek-1.4-exe/bin/README.txt
@@ -0,0 +1,15 @@
+OS X universal binary, 10.3.9 and up
+
+to install (optional):
+    > sudo cp sndpeek /usr/bin/
+    (enter password)
+    > sudo chmod 755 /usr/bin/sndpeek
+
+to run:
+    (from Terminal)
+    > sndpeek
+
+    (or if not installed)
+    > ./sndpeek
+
+it is also possible double click to run
